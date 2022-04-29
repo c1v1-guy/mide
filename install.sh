0ca509307bc6bd2fe141a8d4892d27dc620aecf8
ssleert <semenilichevsky@gmail.com>
2022-04-29 21:25:42 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "micric installation script 0.1"
+echo "1) Arch linux"
+echo "2) Debian"
+echo "3) Fedora"
+read -p "Select needed distro: " distro
+    case $color in
+      "1")
+      	sudo pacman -Sy micro
+      ;;
+      "2")
+      	sudo apt update && sudo apt install micro
+      ;;
+      "3")
+      	sudo dng update && sudo dnf install micro
+      ;;
+    esac
+rm -r $HOME/.config/micro/*
+cp -r staff/* $HOME/.config/micro/
+
